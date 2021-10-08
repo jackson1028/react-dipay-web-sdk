708674c13cc3963f5c1fb1d891bcee9abdf80f81
aldyivans_ <ivanbrutalart@gmail.com>
2021-10-08 16:17:37 +0700

    change library name

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { DipayLogin, DipayPay, getUser } from 'react-dipay'
-import 'react-dipay/dist/index.css'
+import { DipayLogin, DipayPay, getUser } from 'react-dipay-web-sdk'
+import 'react-dipay-web-sdk/dist/index.css'
 
 const clientId = "indofund";
 const secretKey = "dd82f82c148788ed308a15e4eb0d6ede:34d30d7c13886b798f51efa3696739e294fb810d355d8b2d2cf5149fc675f820fcf1e7bef687108a73423f3ea41b2ef7ce095063ec65162e430d6f0335f7dcbab7dba6fade482c71f4fd0dca4ca7062f9bfac4a0c211f340c82c54802cb485521975f93af445d8aee5a879c0001bd870";
